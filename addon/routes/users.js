53be57201f84ced2df80e407c045cb6f0dcdb12e
Scott Harris <scottharris86@me.com>
2017-04-24 17:40:34 -0400

    playing around seeing what i can do 2

--- addon/routes/users.js
+++ addon/routes/users.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
-        debugger;
-        return this.store.findAll('station');
-    }
+    // model() {
+    //     debugger;
+    //     return this.store.findAll('station');
+    // }
 });
