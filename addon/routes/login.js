c3e97c88cdafa7cea8ac6ed3276f66ca50ea0162
Scott Harris <scottharris86@me.com>
2017-04-24 12:05:35 -0400

    Revert "Revert "login form not showing up""
    This reverts commit 3a305fb8a1c4da705965999c38e599d310a4e3d7.

--- addon/routes/login.js
+++ addon/routes/login.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+        isLoggingIn: false,
+
     setupController: function(controller, model) {
         // Call _super for default behavior
         this._super(controller, model);
